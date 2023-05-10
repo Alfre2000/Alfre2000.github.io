51dbd02d45c58346e2d20e554d530eb2aeac8d98
Alfredo <alfredo.mauri.2000@gmail.com>
2023-05-10 16:28:53 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+    '*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
